936d5173cb4683edbbcc1ffeab428737e18a306b
dks07 <dilip.kumar@zingmobile.com.sg>
2024-01-26 21:39:04 +0530

    updates

--- apps/dashboard/models/script.py
+++ apps/dashboard/models/script.py
@@ -1,65 +1,99 @@
+import openpyxl
 import datetime
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "success_tool.settings")
+from django.contrib.auth.models import User
+from apps.dashboard.models.masters import (
+    StatusMaster,
+    SdoMaster,
+    MenuCardMaster,
+    MenuSdoMapping,
+    CustomerMaster,
+    RegionMaster,
+    ProjectMaster,
+    SuccessServiceMaster,
+    PSMMaster,
+    CSMMaster,
+    SDMMaster,
+    IndustryMaster,
+    SuccessElementsMaster,
+    CustomerMapping,
+)
 
-import django
-django.setup()
+def insert_from_excel(filename):
+    try:
+        wb = openpyxl.load_workbook(filename)
+        ws = wb['customer_project']
 
-from apps.dashboard.models.masters import (SdoMaster, MenuCardMaster, MenuSdoMapping, CustomerMaster, RegionMaster,
-                                           ProjectMaster, SuccessServiceMaster, PSMMaster, CSMMaster, SDMMaster,
-                                           IndustryMaster, SuccessElementsMaster, CustomerMapping)
+        # Mapping of column names to their corresponding master models and their respective field names
+        column_mapping = {
+            'Customer': (CustomerMaster, 'customer_name'),
+            'Region': (RegionMaster, 'region_name'),
+            'Customer Id': (CustomerMaster, 'customer_id'),
+            'Project Id': (ProjectMaster, 'project_name'),
+            'Opp No.': None,  # No matching master model found
+            'Success Service': (SuccessServiceMaster, 'success_service_name'),
+            'CSM': (CSMMaster, 'csm_name'),
+            'PSM': (PSMMaster, 'psm_name'),
+            'SDM': (SDMMaster, 'sdm_name'),
+            'Industry': (IndustryMaster, 'industry_type_name'),
+            'Success Elements': (SuccessElementsMaster, 'success_element_name'),
+            'Description': None,  # No matching master model found
+        }
 
+        for row in ws.iter_rows(min_row=2, values_only=True):
+            # Replace blank values with "NA"
+            row = ['NA' if value is None else value for value in row]
 
-def insert_data():
-    menu_card_data = MenuCardMaster.objects.all()
-    # print(menu_card_data,'>>>>>>>>>>')
+            # Fetch related master objects based on the values from the current row
+            master_objects = {}
+            for col_name, col_value in zip(ws[1], row):
+                col_name = col_name.value
+                if col_name in column_mapping and col_value != 'NA':
+                    model, field_name = column_mapping[col_name]
+                    if model is not None:
+                        # Query master objects using the specified field name
+                        try:
+                            master_objects[col_name] = model.objects.filter(**{field_name: col_value}).first()
+                        except model.DoesNotExist:
+                            print(f"Master data not found for {col_name}: {col_value}. Skipping this column.")
+                            break
+                    else:
+                        # No matching master model found, use the column value directly
+                        master_objects[col_name] = col_value
 
-    for mn in menu_card_data:
+            else:
+                # Create CustomerMapping instance if all master data is available
+                customer_mapping_instance = CustomerMapping.objects.create(
+                    customer=master_objects['Customer'],
+                    region=master_objects['Region'],
+                    project=master_objects['Project Id'],  # Use 'Project Id' column
+                    success_service=master_objects['Success Service'],
+                    csm=master_objects.get('CSM'),
+                    psm=master_objects.get('PSM'),
+                    sdm=master_objects.get('SDM'),
+                    industry=master_objects['Industry'],
+                    description=master_objects['Description'],  # Use 'Description' column
+                    created_by=User.objects.first(),  # Replace with appropriate user
+                    created_date=datetime.datetime.now(),
+                    updated_by=User.objects.first(),  # Replace with appropriate user
+                    updated_date=datetime.datetime.now(),
+                    status=StatusMaster.objects.first(),  # Replace with appropriate status
+                )
 
-        if mn.menu_card.startswith("SAA"):
-            sdo = SdoMaster.objects.get(id=1)
-            MenuSdoMapping.objects.create(
-                menu_card_id=mn,
-                sdo_id=sdo,
-                status='1'
-            )
-    return True
+                # Add success elements if available
+                if 'Success Elements' in master_objects:
+                    customer_mapping_instance.success_elements.add(master_objects['Success Elements'])
 
-# a=insert_data()
-# print(a)
+        return True
 
+    except FileNotFoundError:
+        print(f"File not found: {filename}")
+        return False
+    except openpyxl.utils.exceptions.InvalidFileException:
+        print(f"Invalid Excel file: {filename}")
+        return False
+    except Exception as e:
+        print(f"An error occurred while reading the Excel file: {e}")
+        return False
 
-def insert_details():
-    customer = CustomerMaster.objects.all()
-    region = RegionMaster.objects.all()
-    project = ProjectMaster.objects.all()
-    success = SuccessServiceMaster.objects.all()
-    csm = CSMMaster.objects.all()
-    psm = PSMMaster.objects.all()
-    sdm = SDMMaster.objects.all()
-    inds = IndustryMaster.objects.all()
-    succE = SuccessElementsMaster.objects.all()
-
-    for cus, reg, pr, suc, cs, ps, sd, ind, succ in zip(customer,region, project, success, csm, psm, sdm, inds,succE):
-        CustomerMapping.objects.create(
-            customer=cus,
-            region=reg,
-            project=pr,
-            success_service=suc,
-            csm=cs,
-            psm=ps,
-            sdm=sd,
-            industry=ind,
-            success_elements=succ,
-            description='description',
-            created_date=datetime.date.today(),
-            updated_date=datetime.date.today(),
-            status='1'
-
-
-        )
-    return True
-
-
-# test = insert_details()
-# print(test)
+# Call function with Excel filename
+insert_from_excel('E:\\IFS_BACKEND\\success_tool_backend_local\\report_creaton\\apps\\dashboard\\models\\data_mapping_1.xlsx')
